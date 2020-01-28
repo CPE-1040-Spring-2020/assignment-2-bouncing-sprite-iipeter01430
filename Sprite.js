4d644c5643b2d2ec520ebbcc13007880f920f068
iipeter01430 <60205451+iipeter01430@users.noreply.github.com>
2020-01-27 19:48:30 -0700

    Create Sprite.js

---
+++ Sprite.js
@@ -0,0 +1,23 @@
+let item = game.createSprite(2, 2)
+
+item.turn(Direction.Right, 45)
+item.move(2)
+
+let ball = game.createSprite(0, 2)
+basic.showNumber(ball.get(LedSpriteProperty.Brightness));
+
+let light = game.createSprite(2, 2);
+item.turn(Direction.Right, 45);
+
+let bot = game.createSprite(4, 2);
+basic.showNumber(bot.get(LedSpriteProperty.Direction));
+
+input.onButtonPressed(Button.B, () => {
+    ball.ifOnEdgeBounce();
+    basic.showNumber
+}
+
+
+basic.forever(function () {
+
+})
