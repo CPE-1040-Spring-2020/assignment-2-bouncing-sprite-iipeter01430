9e54f2fe89daf9a3a34a008e6f028aa28c4c093a
iipeter01430 <60205451+iipeter01430@users.noreply.github.com>
2020-01-29 18:37:11 -0700

    Update Sprite.js

--- Sprite.js
+++ Sprite.js
@@ -1,9 +1,17 @@
 let ball = game.createSprite(2, 2);
+input.onButtonPressed(Button.B, function () {
+    ball.setX(Math.randomRange(0, 4))
+    ball.setY(Math.randomRange(0, 4))
+})
 
+input.onButtonPressed(Button.A, function () {
+    ball.turn(Direction.Left, 45)
+})
 basic.forever(function () {
     ball.ifOnEdgeBounce();
     ball.move(1);
 
 })
+
 
     
