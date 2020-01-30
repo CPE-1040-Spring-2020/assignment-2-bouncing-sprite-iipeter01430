8440d5c1f0f54f7d5a1754a47d2c487387fe56a8
iipeter01430 <60205451+iipeter01430@users.noreply.github.com>
2020-01-29 18:21:08 -0700

    Update Sprite.js

--- Sprite.js
+++ Sprite.js
@@ -1,30 +1,9 @@
-let item = game.createSprite(2, 2)
+let ball = game.createSprite(2, 2);
 
-item.turn(Direction.Right, 45)
-item.move(2)
+basic.forever(function () {
+    ball.ifOnEdgeBounce();
+    ball.move(1);
 
-let ball = game.createSprite(0, 2)
-basic.showNumber(ball.get(LedSpriteProperty.Brightness));
+})
 
-let light = game.createSprite(2, 2);
-item.turn(Direction.Right, 45);
-
-let bot = game.createSprite(4, 2);
-basic.showNumber(bot.get(LedSpriteProperty.Direction));
-
-input.onButtonPressed(Button.B, () => {
-   ball.ifOnEdgeBounce();
-   basic.showNumber
-  }
-
-
-  basic.forever(function() {
-
-  })
-  
-  let ball = game.createSprite(0, 2);
-basic.pause(200);
-ball.change(LedSpriteProperty.X, 2);
-
-input.onButtonPressed(Button.A, () => {
     
